391e2f200a40d49cd5a8ab754d6ffa6a8dac49bd
jane141806 <huonggiang141806@gmail.com>
2020-04-13 16:02:13 +0700

    first commit

---
+++ src/se1404/SE1404.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se1404;
+
+import util.MyToys;
+
+/**
+ *
+ * @author Dell
+ */
+public class SE1404 {
+    public static void main(String[] args) {
+        System.out.println("5! = "+MyToys.cF(5));
+        System.out.println("6! = "+MyToys.cF(6));
+        System.out.println("0! = "+MyToys.cF(0));
+        System.out.println("-5! = "+MyToys.cF(-5));
+    }
+}
---
+++ src/util/MyToys.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Dell
+ */
+public class MyToys {
+    // cF : compute Factorial, tinh giai thua
+   // class tiện ích dành cho mọi dự án nên dùng statuc
+    // dành cho CI
+    public static long cF(int n){
+        if(n < 0 || n> 15)
+            throw new IllegalArgumentException("n must be between 0..15");
+        if (n == 0) {
+            return 1;
+        }
+        long product = 1; // biến nhân dồn, accumulation: biến tích lũy
+        for(int i = 1 ; i <= n;i++){
+            product *= i;
+        }
+            return product ;
+    }
+}
