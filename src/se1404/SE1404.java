3bbb15d63b6ce863fed1c20bfb33a4f67da1c611
jane141806 <huonggiang141806@gmail.com>
2020-04-16 21:03:44 +0700

    4nd upload: green color

--- src/se1404/SE1404.java
+++ src/se1404/SE1404.java
@@ -13,9 +13,8 @@ import util.MyToys;
  */
 public class SE1404 {
     public static void main(String[] args) {
-        System.out.println("5! = "+MyToys.cF(5));
+        System.out.println("2! = "+MyToys.cF(2));
         System.out.println("6! = "+MyToys.cF(6));
         System.out.println("0! = "+MyToys.cF(0));
-        System.out.println("-5! = "+MyToys.cF(-5));
     }
 }
